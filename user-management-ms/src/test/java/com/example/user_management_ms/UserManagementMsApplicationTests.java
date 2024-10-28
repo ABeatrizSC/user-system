722b4ae24fd6697df791e878dee6fa480448434b
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-10-28 16:05:33 -0300

    feat: create user-management-ms structure

---
+++ user-management-ms/src/test/java/com/example/user_management_ms/UserManagementMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_management_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
