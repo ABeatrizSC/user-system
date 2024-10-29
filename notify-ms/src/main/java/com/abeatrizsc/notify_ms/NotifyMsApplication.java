5a7a2aceb4b68792216ee98323e816812978e2f9
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-10-29 16:17:10 -0300

    feat: create notify-ms structure

---
+++ notify-ms/src/main/java/com/abeatrizsc/notify_ms/NotifyMsApplication.java
@@ -0,0 +1,13 @@
+package com.abeatrizsc.notify_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyMsApplication.class, args);
+	}
+
+}
---
+++ notify-ms/src/test/java/com/abeatrizsc/notify_ms/NotifyMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abeatrizsc.notify_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
